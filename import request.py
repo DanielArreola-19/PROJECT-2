ea47116f3640469822dfd5553ba7852456d70dbc
danielarreola-19 <danielarreola19@gmail.com>
2023-02-09 13:42:28 -0800

    UPDATE 2

--- import request.py
+++ import request.py
@@ -1,2 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 url = "https://sfbay.craigslist.org/"
+# Make a GET request to the website
+response = requests.get(url)
+# Get the content of the response
+content = response.content
+# use beautifulsoup to parse the content
+soup = BeautifulSoup(content,"html.parser")
+# Find all the post titles
+post_titles = soup.findall("a")
+# print the post titles
+for title in post_titles:
+    print(title.text)
